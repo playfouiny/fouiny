966b1536f8d9bd7219154f510f9f3982dd0342ec
steeven irakoze <steevenirakoze@s.lan>
2023-10-04 09:17:03 -0500

    yurrr

---
+++ warmup/src/main/java/org/example/Warmup.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Warmup {
+    public static void main(String[] args) {
+        String personName = "John Doe";
+        int personAge = 25;
+        double productPrice = 19.99;
+        float temperatureCelsius = 27.5f;
+        boolean isLoggedIn = True;
+
+
+
+
+        System.out.println("yurrrrr");
+    }
+}
