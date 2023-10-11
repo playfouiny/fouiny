20e50e22aa9caea8550a49001138abafe80071b0
steeven irakoze <steevenirakoze@s.lan>
2023-10-11 16:01:43 -0500

    Wednesday WarnUp Week 2

---
+++ warmup/WednesdayWarmUpWeek2/src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+  /*      for (int i = 2; i <= 20; i = i + 2)
+        System.out.printf("%d\n",i);
+    }
+}*/
+// this top part shows even number using for loop below i will write  a code for odd number
+        int i=1;
+        while ( i < 20) {
+            System.out.println(i);
+            i = i + 2;
+        }
+    }
+}
